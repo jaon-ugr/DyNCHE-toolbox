4fcf24792832b4bf92785cdecf19ca8f061dc771
jaon-ugr <javolmedo@ugr.es>
2025-07-17 13:30:41 +0200

    Update script.sh

--- main/script.sh
+++ main/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 make clean
 make
-echo alpha-n512-1.txt beta-n512-1.txt norm-n512-1.txt 1 513 12 0.0 10.0 | ./exec 
+echo alpha-n512-1.txt beta-n512-1.txt norm-n512-1.txt 1 513 1 | ./exec 
 
